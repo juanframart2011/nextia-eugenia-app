051891aa6e46569a2f2463eba15f5c187b27320a
Juan Rogelio Franco Martinez <juanframart2011@gmail.com>
2024-02-05 21:30:03 -0600

    finish login, vista de registro y recuperar contraseña

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { RecoveryComponent } from './pages/auth/recovery/recovery.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: "", component: LoginComponent, pathMatch: "full"
+  },
+  {
+    path: 'auth',
+    children: [
+      { path: '', component: LoginComponent },
+      { path: 'recovery', component: RecoveryComponent },
+      { path: 'register', component: RegisterComponent }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AuthService } from './services/auth.service';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AuthGuard } from './auth.guard';
+import { HttpInterceptorService } from './http-interceptor';
+import { FormsModule } from '@angular/forms';
+import { RecoveryComponent } from './pages/auth/recovery/recovery.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    FooterComponent,
+    RecoveryComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    AuthGuard,{
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpInterceptorService,
+      multi: true,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
