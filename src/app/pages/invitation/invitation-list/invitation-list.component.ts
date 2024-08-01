0d1f9bc8e1055e8fb22f754a2d49dfdaac69b450
Juan Rogelio Franco Martinez <juanframart2011@gmail.com>
2024-08-01 17:41:38 -0600

    ajuste de paginacion y buscador

--- src/app/pages/invitation/invitation-list/invitation-list.component.ts
+++ src/app/pages/invitation/invitation-list/invitation-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Invitation } from 'src/app/interface/invitation.interface';
+import { InvitationService } from 'src/app/services/invitation.service';
 
 @Component({
   selector: 'app-invitation-list',
@@ -9,33 +10,77 @@ import { Invitation } from 'src/app/interface/invitation.interface';
 export class InvitationListComponent {
 
   invitations:Invitation[] = [];
+  invitationsAll:Invitation[] = [];
   loadingInvitations:boolean = true;
   currentPage:number = 0;
   searchText:string = '';
+  limit:number = 10;
+  totalPages:number = 0;
 
-  constructor(){
+  constructor(private invitationService:InvitationService){}
 
+  private _getList(){
+    this.invitationService.getAll( this.currentPage, this.limit ).subscribe((data)=>{
+      
+      this.invitations = data.results;
+      this.invitationsAll = this.invitations;
+      this.totalPages = data.total;
+      this.loadingInvitations = false;
+    });
   }
 
-  callSearch(){}
+  private _search(){
+    
+    this.invitations = this.invitationsAll.filter((invitation) => {
+      
+      const searchLower = this.searchText.toLowerCase();
+      const nameMatch = invitation.name.toLowerCase().includes(searchLower);
+      const entrada = invitation.entry_date.toLowerCase().includes(searchLower);
+      
+      // Retorna true si alguna de las comprobaciones es verdadera
+      return nameMatch || entrada;
+    });
+  }
+
+  ngOnInit(){
+    this._getList();
+  }
+
+  callSearch(){
+    
+    if( this.searchText ){
+      this._search();
+    }
+    else{
+      this.invitations = this.invitationsAll;
+    }
+  }
 
   delete(invitation:Invitation){}
 
   detail(invitationId:number){}
 
-  getPages():Number[]{
-    return Array(0,1,2);
+  getPages(): number[] {
+    const pages = [];
+    for (let i = 1; i <= this.totalPages; i++) {
+      pages.push(i);
+    }
+    return pages;
   }
 
-  goToPage(page:any){
-
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
   }
-
-  nextPage(){}
-
-  previousPage(){}
-
-  totalPages():number{
-    return 0;
+  
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+    }
+  }
+  
+  goToPage(page: number) {
+    this.currentPage = page;
   }
 }
